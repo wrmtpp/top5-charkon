47b17995a544632c79953eab1a9b3db0cedf960b
wrmtpp <woramet2539@icloud.com>
2019-09-17 16:36:25 +0700

    Updates

---
+++ precache-manifest.90cfdb000e95f36ee1eb13ba382ff0d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca1efbbdc7f582004d004310dd2790d",
+    "url": "/top5-charkon/index.html"
+  },
+  {
+    "revision": "bce184f7ad6527b3d01e",
+    "url": "/top5-charkon/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "2479e19ac5e846affb84",
+    "url": "/top5-charkon/static/css/main.682e05e1.chunk.css"
+  },
+  {
+    "revision": "bce184f7ad6527b3d01e",
+    "url": "/top5-charkon/static/js/2.0951ce2a.chunk.js"
+  },
+  {
+    "revision": "2479e19ac5e846affb84",
+    "url": "/top5-charkon/static/js/main.5a20a01e.chunk.js"
+  },
+  {
+    "revision": "abf896bc7a261c52f7a1",
+    "url": "/top5-charkon/static/js/runtime~main.647a20e3.js"
+  }
+]);
