0bc5b60c116fb2736d39f4651812112d0f76ae06
wrmtpp <woramet2539@icloud.com>
2019-09-17 13:59:45 +0700

    ASM1

---
+++ build/precache-manifest.9a0f9c98224227db6b74a0aeae98eda0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38afb1cccd422981c8ef4a1f09713c91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bce184f7ad6527b3d01e",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "633f2e307f80a572cd40",
+    "url": "/static/css/main.682e05e1.chunk.css"
+  },
+  {
+    "revision": "bce184f7ad6527b3d01e",
+    "url": "/static/js/2.0951ce2a.chunk.js"
+  },
+  {
+    "revision": "633f2e307f80a572cd40",
+    "url": "/static/js/main.f28c55e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
